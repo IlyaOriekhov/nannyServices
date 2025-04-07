07318093d487ba73120e3e09200e09e2ab3bac97
Ilya Oriekhov <164690908+IlyaOriekhov@users.noreply.github.com>
2025-04-07 19:13:27 +0300

    add app and main plus styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
-function App() {}
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const NanniesPage = lazy(() => import("./pages/NanniesPage/NanniesPage"));
+const FavoritePage = lazy(() => import("./pages/FavoritePage/FavoritePage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/nannies" element={<NanniesPage />} />
+        <Route path="/favorites" element={<FavoritePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
+}
 
 export default App;
